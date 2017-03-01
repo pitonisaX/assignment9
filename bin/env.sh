d6aadc063b527151eb1dda0ac99f033e2e66d9fb
pitonisaX <karreyes@gmail.com>
2017-02-28 19:52:49 -0800

    setting up travis

--- bin/env.sh
+++ bin/env.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # Setup Project Specfics - No Secrets
-export DOCKER_REPO=pdxdiver
+export DOCKER_REPO=pitonisax
 export DOCKER_IMAGE=homeless-service
 export PROJ_SETTINGS_DIR=homelessAPI
 export DEPLOY_TARGET=LOCAL
